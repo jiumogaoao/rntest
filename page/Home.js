13d5a7ffa99b5f317e8334ccbc35a4099c024868
jiumogaoao <394127821@qq.com>
2018-06-18 07:43:10 +0800

    change

--- page/Home.js
+++ page/Home.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View,AppRegistry,TouchableOpacity,Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import AsyncComponent from '../components/AsyncComponent';
 import IconFont from '../components/IconFont';
+const Swaper = asyncComponent(() => import("../components/Swaper"));
 export default class Home extends Component {
   render() {
     return (
       <View>
-        <View style={{height:parseInt(490*w)}}>
-          
-        </View>
+        <Swaper/>
       </View>
     )
     }
